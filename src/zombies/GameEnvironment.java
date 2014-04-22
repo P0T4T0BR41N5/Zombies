8425cee963c69287cca231bd5583faf786409424
KrishIsBaws <Achyuth1998@rusty-pc.brentwood.bc.ca>
2014-04-22 10:19:33 -0700

    zombie follow

--- src/zombies/GameEnvironment.java
+++ src/zombies/GameEnvironment.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package zombies;
 
 import environment.Environment;
@@ -20,51 +19,65 @@ import path.TrigonometryCalculator;
  * @author Leo
  */
 class GameEnvironment extends Environment implements MouseMotionListener {
+
     private Character hero;
+    private Zombie zombie;
     private Crosshair crosshair;
-    int characterSpeed;
-    
-    
+    private int characterSpeed;
+    private int zombieSpeed;
+
     @Override
     public void initializeEnvironment() {
-        
-        characterSpeed = 3;
-        
-        hero = new Character(new Point(100, 100), new Velocity(0, 0));
-        this.getActors().add(hero);
-        
-        crosshair = new Crosshair(new Point(100, 100), new Velocity(0, 0));
-        this.getActors().add(crosshair);
-        
+
+        setCharacterSpeed(3);
+        setZombieSpeed(2);
+
+        setHero(new Character(new Point(100, 100), new Velocity(0, 0)));
+        this.getActors().add(getHero());
+
+        setCrosshair(new Crosshair(new Point(100, 100), new Velocity(0, 0)));
+        this.getActors().add(getCrosshair());
+
         this.getActors().add(new Zombie(new Point(10, 10), new Velocity(0, 0)));
         addMouseMotionListener(this);
+
+        setZombie(new Zombie(new Point(this.randomPoint()), new Velocity(0, 0)));
+        this.getActors().add(getZombie());
+
+
+    }
+
+    private Point randomPoint() {
+        return new Point((int) (Math.random() * 100), (int) (Math.random() * 100));
+
     }
 
     @Override
     public void timerTaskHandler() {
-
+        zombie.setVelocity(TrigonometryCalculator.calculateVelocity(zombie.getPosition(), hero.getPosition(), 2));
     }
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_A) {
-            hero.setVelocity(new Velocity(-characterSpeed, 0));
+            getHero().setVelocity(new Velocity(-getCharacterSpeed(), 0));
         } else if (e.getKeyCode() == KeyEvent.VK_D) {
-            hero.setVelocity(new Velocity(characterSpeed, 0));
+            getHero().setVelocity(new Velocity(getCharacterSpeed(), 0));
         } else if (e.getKeyCode() == KeyEvent.VK_W) {
-            hero.setVelocity(new Velocity(0, -characterSpeed));
+            getHero().setVelocity(new Velocity(0, -getCharacterSpeed()));
         } else if (e.getKeyCode() == KeyEvent.VK_S) {
-            hero.setVelocity(new Velocity(0, characterSpeed));
+            getHero().setVelocity(new Velocity(0, getCharacterSpeed()));
         }
     }
 
     @Override
     public void keyReleasedHandler(KeyEvent e) {
-        if ((e.getKeyCode() == KeyEvent.VK_A) ||
-            (e.getKeyCode() == KeyEvent.VK_D) ||
-            (e.getKeyCode() == KeyEvent.VK_W) ||
-            (e.getKeyCode() == KeyEvent.VK_S)) {
-         hero.stop();
+        if ((e.getKeyCode() == KeyEvent.VK_A)
+                || (e.getKeyCode() == KeyEvent.VK_D)
+                || (e.getKeyCode() == KeyEvent.VK_W)
+                || (e.getKeyCode() == KeyEvent.VK_S)) {
+            getHero().stop();
+
         }
     }
 
@@ -82,22 +95,89 @@ class GameEnvironment extends Environment implements MouseMotionListener {
     @Override
     public void mouseDragged(MouseEvent e) {
     }
-    
+
     @Override
     public void mouseMoved(MouseEvent e) {
-        if (crosshair != null) {
-         crosshair.setPosition(new Point (e.getPoint().x - 15, e.getPoint().y - 15));
-         
-         hero.setAngle((int) (TrigonometryCalculator.calculateAngle(hero.getCenterOfMass(), crosshair.getCenterOfMass()) * 360) );
-         
-         System.out.println("hero angle = " + TrigonometryCalculator.calculateAngle(hero.getCenterOfMass(), crosshair.getCenterOfMass()));
-         System.out.println("hero angle = " + hero.getAngle());
-         
-         
+        if (getCrosshair() != null) {
+            getCrosshair().setPosition(new Point(e.getPoint().x - 15, e.getPoint().y - 15));
+
+            getHero().setAngle((int) (TrigonometryCalculator.calculateAngle(getHero().getCenterOfMass(), getCrosshair().getCenterOfMass()) * 360));
+
+            System.out.println("hero angle = " + TrigonometryCalculator.calculateAngle(getHero().getCenterOfMass(), getCrosshair().getCenterOfMass()));
+            System.out.println("hero angle = " + getHero().getAngle());
+
         }
     }
 //</editor-fold>
 
+    /**
+     * @return the hero
+     */
+    public Character getHero() {
+        return hero;
+    }
 
-    
+    /**
+     * @param hero the hero to set
+     */
+    public void setHero(Character hero) {
+        this.hero = hero;
+    }
+
+    /**
+     * @return the zombie
+     */
+    public Zombie getZombie() {
+        return zombie;
+    }
+
+    /**
+     * @param zombie the zombie to set
+     */
+    public void setZombie(Zombie zombie) {
+        this.zombie = zombie;
+    }
+
+    /**
+     * @return the crosshair
+     */
+    public Crosshair getCrosshair() {
+        return crosshair;
+    }
+
+    /**
+     * @param crosshair the crosshair to set
+     */
+    public void setCrosshair(Crosshair crosshair) {
+        this.crosshair = crosshair;
+    }
+
+    /**
+     * @return the characterSpeed
+     */
+    public int getCharacterSpeed() {
+        return characterSpeed;
+    }
+
+    /**
+     * @param characterSpeed the characterSpeed to set
+     */
+    public void setCharacterSpeed(int characterSpeed) {
+        this.characterSpeed = characterSpeed;
+    }
+
+    /**
+     * @return the zombieSpeed
+     */
+    public int getZombieSpeed() {
+        return zombieSpeed;
+    }
+
+    /**
+     * @param zombieSpeed the zombieSpeed to set
+     */
+    public void setZombieSpeed(int zombieSpeed) {
+        this.zombieSpeed = zombieSpeed;
+    }
+
 }
