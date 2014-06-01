d5318b1e98cad456c43c1e1fe6ff81297df63783
l19brandon <l19brandon@hotmail.com>
2014-06-01 12:11:53 -0700

    zombie death sound

--- src/zombies/Zombie.java
+++ src/zombies/Zombie.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package zombies;
 
+import audio.AudioPlayer;
 import environment.Actor;
 import environment.Velocity;
 import images.ResourceTools;
@@ -17,35 +17,33 @@ import java.awt.Point;
  *
  * @author Leo
  */
-
 public class Zombie extends Actor {
-    
+
 //<editor-fold defaultstate="collapsed" desc="Constructor / Initialization">
-    private void initialize(){
+    private void initialize() {
         this.setImage(ResourceTools.loadImageFromResource("resources/zombie.png"));
     }
-    
+
     public Zombie(Point position, Velocity velocity) {
         super(position, velocity);
         initialize();
     }
 //</editor-fold>
-    
+
 //<editor-fold defaultstate="collapsed" desc="Properties">
     public static int MIN_HEALTH = 0;
     public static int MAX_HEALTH = 100;
-    
-    
+
     private boolean alive = true;
-    private int health = 100;    
-    
+    private int health = 100;
+
     /**
      * @return the Health
      */
     public int getHealth() {
         return health;
     }
-    
+
     /**
      * @param health the Health to set
      */
@@ -56,34 +54,48 @@ public class Zombie extends Actor {
         }
         this.health = health;
     }
-    
+
     /**
      * @param health the Health to set
      */
     public void addToHealth(int health) {
         setHealth(this.health + health);
     }
-    
+
     /**
      * @return the alive
      */
     public boolean isAlive() {
         return alive;
     }
-    
+
     /**
      * @param alive the alive to set
      */
     public void setAlive(boolean alive) {
         if ((this.alive) && !(alive)) {
-            //play dying sound
-            //maybe change image to dead guy on ground
+            if (Math.random() < 0.2) {
+                AudioPlayer.play("/resources/zombie_growl_1.wav");
+
+            } else if (Math.random() < 0.4) {
+                AudioPlayer.play("/resources/zombie_growl_2.wav");
+
+            } else if (Math.random() < 0.6) {
+                AudioPlayer.play("/resources/zombie_growl_3.wav");
+
+            } else if (Math.random() < 0.8) {
+                AudioPlayer.play("/resources/zombie_growl_4.wav");
+
+            } else {
+                AudioPlayer.play("/resources/zombie_growl_5.wav");
+
+            }
             this.stop();
         }
         this.alive = alive;
     }
 //</editor-fold>
-    
+
 //<editor-fold defaultstate="collapsed" desc="Painting">
     @Override
     public void paint(Graphics graphics) {
@@ -96,5 +108,5 @@ public class Zombie extends Actor {
         graphics.fillRect(this.getPosition().x, this.getPosition().y, 30 * health / MAX_HEALTH, 5);
     }
 //</editor-fold>
-    
+
 }
